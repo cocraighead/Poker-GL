c0586ae899247d151a02d6cc1c0ba047a2fb2742
cocraighead <colincraighead@yahoo.com>
2024-02-07 23:37:28 -0700

    getting close on bent card

--- Misc Docs/mathbentcard.js
+++ Misc Docs/mathbentcard.js
@@ -19,15 +19,16 @@ function addColor(point){
     point.push(.5)
 }
 
-function addTexture(point,isLeft){
-    var T = isLeft ? .5 : 1
-    var S = (point[3] + 1) / 2
+function addTexture(point,T,isLeft,faceUp){
+    var S = isLeft ?
+        faceUp ? .5 : 0
+        : faceUp ? 1 : .5
 
     point.push(S)
     point.push(T)
 }
 
-function fatten2DArray(_2dArray){
+function flatten2DArray(_2dArray){
     var d1 = _2dArray.length
     var d2 = d1 ? _2dArray[0].length : 0
 
@@ -41,29 +42,32 @@ function fatten2DArray(_2dArray){
     return flatArr
 }
 
-function generateBuffer()
-{
+function generateBentFace(isTop){
     var buffer = []
     var numCuts = 100
+    var cutFraction = 1/numCuts
     var z_start = -1
     var z_end = 1
     var z_diff = z_end - z_start
     var z_delta = z_diff / numCuts
 
+    var y_shift = isTop ? .001 : -.001
+
     for(var i=0;i<numCuts;i++){
         var z1 = z_start + i * z_delta
         var z2 = z_start + (i+1) * z_delta
 
-        var y1 = 10 ** (z1 - 1)
-        var y2 = 10 ** (z2 - 1)
+        var y1 = 10 ** (z1 - 1) + y_shift
+        var y2 = 10 ** (z2 - 1) + y_shift
 
         var point1 = [-1, y1, z1]
         var point2 = [1, y1, z1]
         var point3 = [-1, y2, z2]
 
         var point4 = [1, y1, z1]
-        var point5 = [-1, y2, z2]
-        var point6 = [1, y2, z2]
+        var point5 = [1, y2, z2]
+        var point6 = [-1, y2, z2]
+        
 
         addNormal(point1)
         addNormal(point2)
@@ -79,12 +83,86 @@ function generateBuffer()
         addColor(point5)
         addColor(point6)
 
-        addTexture(point1,true)
-        addTexture(point2,false)
-        addTexture(point3,true)
-        addTexture(point4,false)
-        addTexture(point5,true)
-        addTexture(point6,false)
+        if(isTop){
+            addTexture(point2,i*cutFraction,false,!isTop)
+            addTexture(point1,i*cutFraction,true,!isTop)
+            addTexture(point3,(i+1)*cutFraction,true,!isTop)
+            addTexture(point5,(i+1)*cutFraction,true,!isTop)
+            addTexture(point4,i*cutFraction,false,!isTop)
+            addTexture(point6,(i+1)*cutFraction,false,!isTop)
+
+            buffer.push(point2)
+            buffer.push(point1)
+            buffer.push(point3)
+            buffer.push(point5)
+            buffer.push(point4)
+            buffer.push(point6)
+        }else{
+            addTexture(point1,i*cutFraction,!true,!isTop)
+            addTexture(point2,i*cutFraction,!false,!isTop)
+            addTexture(point3,(i+1)*cutFraction,!true,!isTop)
+            addTexture(point4,i*cutFraction,!false,!isTop)
+            addTexture(point5,(i+1)*cutFraction,!false,!isTop)
+            addTexture(point6,(i+1)*cutFraction,!true,!isTop)
+    
+            buffer.push(point1)
+            buffer.push(point2)
+            buffer.push(point3)
+            buffer.push(point4)
+            buffer.push(point5)
+            buffer.push(point6)
+        }
+    }
+    return flatten2DArray(buffer)
+}
+
+function generateSideFace(isRight){
+    var buffer = []
+    var numCuts = 100
+    var z_start = -1
+    var z_end = 1
+    var z_diff = z_end - z_start
+    var z_delta = z_diff / numCuts
+
+    var x_shift = isRight ? 1 : -1
+
+    var y_shift = .001
+
+    for(var i=0;i<numCuts;i++){
+        var z1 = z_start + i * z_delta
+        var z2 = z_start + (i+1) * z_delta
+
+        var y1 = 10 ** (z1 - 1)
+        var y2 = 10 ** (z2 - 1)
+
+        var point1 = [x_shift, y1-y_shift, z1]
+        var point2 = [x_shift, y1+y_shift, z1]
+        var point3 = [x_shift, y2+y_shift, z2]
+
+        var point4 = [x_shift, y2+y_shift, z2]
+        var point5 = [x_shift, y2-y_shift, z2]
+        var point6 = [x_shift, y1-y_shift, z1]
+
+        addNormal(point1)
+        addNormal(point2)
+        addNormal(point3)
+        addNormal(point4)
+        addNormal(point5)
+        addNormal(point6)
+
+        addColor(point1)
+        addColor(point2)
+        addColor(point3)
+        addColor(point4)
+        addColor(point5)
+        addColor(point6)
+
+        addTexture(point1,0,true,false)
+        addTexture(point2,0,false,false)
+        addTexture(point3,0,true,false)
+        addTexture(point4,0,false,false)
+        addTexture(point5,0,true,false)
+        addTexture(point6,0,false,false)
 
         buffer.push(point1)
         buffer.push(point2)
@@ -93,8 +171,122 @@ function generateBuffer()
         buffer.push(point5)
         buffer.push(point6)
     }
-    console.log('Number of points', buffer.length)
-    return fatten2DArray(buffer)
+    return flatten2DArray(buffer)
+}
+
+function generateFront(){
+    var buffer = []
+    var numCuts = 100
+    var z_start = -1
+    var z_end = 1
+    var z_diff = z_end - z_start
+    var z_delta = z_diff / numCuts
+
+    var y_shift = .001
+
+    for(var i=0;i<numCuts;i++){
+        var z2 = z_start + (i+1) * z_delta
+
+        var y2 = 10 ** (z2 - 1)
+    }
+
+    var point1 = [-1, y2+y_shift, 1]
+    var point2 = [-1, y2-y_shift, 1]
+    var point3 = [+1, y2+y_shift, 1]
+
+    var point4 = [-1, y2-y_shift, 1]
+    var point5 = [+1, y2-y_shift, 1]
+    var point6 = [+1, y2+y_shift, 1]
+
+    addNormal(point1)
+    addNormal(point2)
+    addNormal(point3)
+    addNormal(point4)
+    addNormal(point5)
+    addNormal(point6)
+
+    addColor(point1)
+    addColor(point2)
+    addColor(point3)
+    addColor(point4)
+    addColor(point5)
+    addColor(point6)
+
+    addTexture(point1,0,true,false)
+    addTexture(point2,0,false,false)
+    addTexture(point3,0,true,false)
+    addTexture(point4,0,false,false)
+    addTexture(point5,0,true,false)
+    addTexture(point6,0,false,false)
+
+    buffer.push(point1)
+    buffer.push(point2)
+    buffer.push(point3)
+    buffer.push(point4)
+    buffer.push(point5)
+    buffer.push(point6)
+    return flatten2DArray(buffer)
+}
+
+function generateBack(){
+    var buffer = []
+
+    var y_shift = .001
+
+    var point1 = [+1, -y_shift, -1]
+    var point2 = [-1, +y_shift, -1]
+    var point3 = [+1, +y_shift, -1]
+
+    var point4 = [+1, -y_shift, -1]
+    var point5 = [-1, -y_shift, -1]
+    var point6 = [-1, +y_shift, -1]
+
+    addNormal(point1)
+    addNormal(point2)
+    addNormal(point3)
+    addNormal(point4)
+    addNormal(point5)
+    addNormal(point6)
+
+    addColor(point1)
+    addColor(point2)
+    addColor(point3)
+    addColor(point4)
+    addColor(point5)
+    addColor(point6)
+
+    addTexture(point1,0,true,false)
+    addTexture(point2,0,false,false)
+    addTexture(point3,0,true,false)
+    addTexture(point4,0,false,false)
+    addTexture(point5,0,true,false)
+    addTexture(point6,0,false,false)
+
+    buffer.push(point1)
+    buffer.push(point2)
+    buffer.push(point3)
+    buffer.push(point4)
+    buffer.push(point5)
+    buffer.push(point6)
+    return flatten2DArray(buffer)
+}
+
+function generateBuffer()
+{
+    var lowerBentFace = generateBentFace(false)
+    var upperBentFace = generateBentFace(true)
+    var leftFace = generateSideFace(false)
+    var rightFace = generateSideFace(true)
+    var frontFace =  generateFront()
+    var backFace = generateBack()
+
+    var faces = [lowerBentFace,upperBentFace,leftFace,rightFace,frontFace,backFace]
+    // var faces = [lowerBentFace,upperBentFace,leftFace,rightFace]
+    var buffer = []
+    for(var f=0;f<faces.length;f++){
+        buffer = buffer.concat(faces[f])
+    }
+    return buffer
 }
 
 function writeToFile(arr,valuesInRow){
@@ -120,4 +312,5 @@ function writeToFile(arr,valuesInRow){
 }
 
 var r = generateBuffer()
+console.log(r.length)
 writeToFile(r,11)
